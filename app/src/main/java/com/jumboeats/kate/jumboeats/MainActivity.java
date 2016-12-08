76ae15c7d5961b9b20345ac2c4dcabdb7d5c09a1
Kate Harwood <kateharwood>
2016-12-07 23:31:41 -0500

    adding in date and time parsing for facebook data

--- app/src/main/java/com/jumboeats/kate/jumboeats/MainActivity.java
+++ app/src/main/java/com/jumboeats/kate/jumboeats/MainActivity.java
@@ -6,14 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
-import io.fabric.sdk.android.services.common.Crash;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,6 +26,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,29 +45,16 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<HashMap<String, String>> eventList = new ArrayList<>();
 
     public class GetData extends AsyncTask<Void, Integer, JSONArray> {
-        // private final Context copyOfContext;
 
         public GetData() {
             super();
-//            copyOfContext = context;
         }
-
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-////            TextView date = (TextView) findViewById(R.id.date);
-////            TextView time = (TextView) findViewById(R.id.time);
-////            TextView food = (TextView) findViewById(R.id.food);
-////            TextView sponsor = (TextView) findViewById(R.id.sponsor);
-////            TextView location = (TextView) findViewById(R.id.location);
-//////                 other = (TextView)findViewById(R.id.other);
-//
-//        }
 
 
         //    @Override
         protected JSONArray doInBackground(Void... params) {
             try {
-                URL api = new URL("http://jumboeats.herokuapp.com/");
+                URL api = new URL("https://jumboeats.herokuapp.com/");
                 HttpURLConnection conn = (HttpURLConnection)api.openConnection();
 
                 InputStream is = conn.getInputStream();
@@ -78,22 +66,18 @@ public class MainActivity extends AppCompatActivity {
                 }
                 r.close();
                 is.close();
-//                Log.e("get response", String.valueOf(conn.getResponseCode()));
                 return new JSONArray(total.toString());
             }
             catch (MalformedURLException e) {
                 Crashlytics.logException(e);
-//                Log.e("doInBackground(): ", e.toString());
                 return null;
             }
             catch (IOException e) {
                 Crashlytics.logException(e);
-//                Log.e("doInBackground(): ", e.toString());
                 return null;
             }
             catch (JSONException e) {
                 Crashlytics.logException(e);
-//                Log.e("doInBackground(): ", e.toString());
                 return null;
             }
         }
@@ -113,23 +97,70 @@ public class MainActivity extends AppCompatActivity {
                         String food = entry.getString(TAG_FOOD);
                         String sponsor = entry.getString(TAG_SPONSOR);
                         String location = entry.getString(TAG_LOCATION);
-//                            String other = entry.getString(TAG_OTHER);
-
 
                         HashMap<String, String> map = new HashMap<>();
-                        map.put(TAG_DATE, date);
-                        map.put(TAG_TIME, time);
+
+                        // parse date passed in from database
+                        Date newDate = new Date();
+                        try {
+                            newDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                            String dateString = newDate.toString();
+                            String[] sp = dateString.split(" ");
+                            // System.out.println(sp[1] + " " + sp[2]);
+                            dateString = sp[1] + " " + sp[2];
+                            map.put(TAG_DATE, dateString);
+                        } catch (ParseException e) {
+                            map.put(TAG_DATE, date);
+                        }
+
+                        // parse time passed in from database
+                        Date newTime = new Date();
+                        try {
+                            newTime = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss+SSSS").parse(time);
+                            String timeString = newTime.toString();
+                            String[] sp = timeString.split(" ");
+                            timeString = sp[3];
+                            timeString = timeString.substring(0, 5);
+
+                            Integer hour = 0;
+                            String addOn = "am";
+                            if (!(Integer.parseInt(Character.toString(timeString.charAt(0))) == 0)) {
+
+                              if (Integer.parseInt(Character.toString(timeString.charAt(0))) == 1 ||
+                                        Integer.parseInt(Character.toString(timeString.charAt(0))) == 2) {
+                                hour = Integer.parseInt(timeString.substring(0, 2));
+                                if (hour >= 12) {
+                                    addOn = "pm";
+                                    hour = Integer.parseInt(timeString.substring(0, 2)) - 12;
+                                }
+                              }
+                              timeString = Integer.toString(hour) + timeString.substring(2, timeString.length()) + addOn;
+
+
+                            }
+                            else  {
+                                if (Integer.parseInt(Character.toString(timeString.charAt(1))) == 0) {
+                                    hour = 12;
+                                    timeString = Integer.toString(hour) + timeString.substring(2, timeString.length()) + addOn;
+                                }
+                                timeString = timeString.substring(1, 5) + addOn;
+                            }
+                            map.put(TAG_TIME, timeString);
+                        } catch (ParseException e) {
+                            map.put(TAG_TIME, time);
+                        }
+
+
                         map.put(TAG_FOOD, food);
                         map.put(TAG_SPONSOR, sponsor);
                         map.put(TAG_LOCATION, location);
-//                            map.put(TAG_OTHER, other);
 
                         eventList.add(0, map);
                         list = (ListView) findViewById(R.id.list);
 
                         ListAdapter adapter = new SimpleAdapter(MainActivity.this, eventList,
                                 R.layout.list_item, new String[]{TAG_DATE, TAG_TIME, TAG_FOOD, TAG_SPONSOR, TAG_LOCATION, TAG_OTHER},
-                                new int[]{R.id.date, R.id.time, R.id.food, R.id.sponsor, R.id.location /*R.id.other*/});
+                                new int[]{R.id.date, R.id.time, R.id.food, R.id.sponsor, R.id.location});
 
                         assert list != null;
                         list.setAdapter(adapter);
